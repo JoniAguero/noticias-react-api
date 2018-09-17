3fc4f0b4a6262a06fc125ea7a6f95c4f5cd629f8
JoniAguero <aguerojonathanivan@gmail.com>
2018-09-17 20:25:29 -0300

    get Noticias

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react';
+import { Noticias } from "./components/Noticias";
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      noticias: {}
+    }
+
+  }
+
+  componentDidMount() {
+    this.getNoticias();
+  }
+
+  getNoticias() {
+    const url = 'https://newsapi.org/v2/top-headlines?' +
+      'country=ar&' +
+      'apiKey=1d01268cb056446083eefeee93e25036';
+    const req = new Request(url);
+    fetch(req)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(noticias => { 
+        this.setState({
+          noticias: noticias.articles
+        })
+       });
+  }
+
   render() {
     return (
       <div className="App">
-
+        <Noticias />
       </div>
     );
   }
---
+++ src/components/Noticias.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Noticias extends Component {
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Noticias;
